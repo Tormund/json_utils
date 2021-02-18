acc3a622af816f0d16fe8823221c3cb3f670e7dc
tormund <tormund@oftengames.com>
2021-02-18 12:30:03 +0200

    exclude

--- src/json_utils.nim
+++ src/json_utils.nim
@@ -10,7 +10,14 @@ proc dot(k1, k2: string): string =
   else:
     result = k1 & "." & k2
 
-proc flat*(j: JsonNode, prefix = "", flatArrays: bool = false, objDepth: int = -1): JsonNode =
+type PairsResult = proc(pairs: openarray[Pair]): JsonNode
+
+proc defaultFlatResult(pairs: openarray[Pair]): JsonNode =
+  result = newJObject()
+  for pair in pairs:
+    result[pair.key] = pair.node.copy
+
+proc flatAUX(j: JsonNode, prefix = "", flatArrays: bool = false, objDepth: int = -1, pairsResult: PairsResult): JsonNode =
   var rPairs: seq[Pair]
   var nodesToProcess: seq[Pair]
   nodesToProcess.add((key: prefix, node: j))
@@ -33,12 +40,10 @@ proc flat*(j: JsonNode, prefix = "", flatArrays: bool = false, objDepth: int = -
     nodesToProcess = nextNodes
     inc depth
 
-  # rPairs.sort do(p1, p2: Pair) -> int:
-  #   result = cmp(p1.key, p2.key)
+  result = pairsResult(rPairs)
 
-  result = newJObject()
-  for pair in rPairs:
-    result[pair.key] = pair.node.copy
+proc flat*(j: JsonNode, prefix = "", flatArrays: bool = false, objDepth: int = -1, flatResult = defaultFlatResult): JsonNode =
+  result = j.flatAUX(prefix, flatArrays, objDepth, flatResult)
 
 proc unflat*(j: JsonNode): JsonNode =
   result = newJObject()
@@ -83,3 +88,43 @@ proc exclude*(j: JsonNode, keys: openarray[string]): JsonNode =
     if k notin keys:
       result[k] = v.copy
 
+proc match(k, mask: string): bool =
+  if mask.len > k.len: return false
+  var ki, mi: int
+  result = true
+  while mi < mask.len:
+    if mask[mi] == '*':
+      while ki < k.len and k[ki] != '.':
+        inc ki
+      inc mi
+    if mask[mi] != k[ki]:
+      result = false
+    if ki >= k.len - 1: break
+    inc ki
+    inc mi
+
+proc matchAny(k: string, masks: openarray[string]): bool =
+  for mask in masks:
+    if k.match(mask):
+      return true
+
+proc flatWithExclude(j: JsonNode, keys: openarray[string]): JsonNode =
+  let keys = @keys
+  result = j.flatAUX("", true, -1) do(pairs: openarray[Pair])->JsonNode:
+    result = newJObject()
+    for pair in pairs:
+      if not pair.key.matchAny(keys):
+        result[pair.key] = pair.node.copy
+
+proc excludeEx*(j: JsonNode, keys: openarray[string]): JsonNode =
+  var fj = j.flatWithExclude(keys)
+  result = fj.unflat()
+
+proc flatSorted*(j: JsonNode, prefix = "", flatArrays: bool = false, objDepth: int = -1, cmp: proc(k1,k2: string):int = system.cmp): JsonNode =
+  result = j.flatAUX(prefix, flatArrays, objDepth) do(pairs: openarray[Pair])->JsonNode:
+    var pairs = @pairs
+    pairs.sort do(p1, p2: Pair) -> int:
+      result = cmp(p1.key, p2.key)
+    result = newJObject()
+    for pair in pairs:
+      result[pair.key] = pair.node.copy
